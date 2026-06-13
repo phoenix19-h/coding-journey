6b78c9a0eddf3d386c8149e7bb9cdbff672a2380
Phoenix <phoenix19-h@users.noreply.github.com>
2026-06-13 19:40:01 +0530

    Day 1 complete

---
+++ Python/practice_day1.py
@@ -0,0 +1,18 @@
+# Question 1
+name= input("Enter Name :")
+print(name)
+
+# Question 2
+num1= int(input(" Enter number 1:"))
+num2= int(input(" Enter number 2:"))
+choose=input("operator:")
+if choose=='+':
+    print(num1+num2)
+if choose=='*':
+    print(num1*num2)
+if choose=='/':
+    print(num1/num2)
+if choose=='-':
+    print(num1-num2)
+    
+# Question 3
